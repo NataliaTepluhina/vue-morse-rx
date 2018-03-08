28e4e3633fd4bb1cbc084a05ff1ac8eead4d6623
Natalia Tepluhina <tarya.se@gmail.com>
2018-03-08 15:26:09 +0200

    * added project template

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-app id="app">
+        <v-navigation-drawer clipped fixed app/>
+        <v-toolbar app fixed clipped-left class="deep-purple lighten-2 deep-purple--text text--darken-4">
+            <v-toolbar-side-icon class="deep-purple--text text--darken-4"/>
+            <v-toolbar-title>Morse code decoder</v-toolbar-title>
+            <v-spacer/>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn to="/" flat>
+                    Home
+                </v-btn>
+                <v-btn to="/about" flat>
+                   About
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <router-view/>
+            </v-container>
+        </v-content>
+        <v-footer app/>
+    </v-app>
+</template>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Vuetify from 'vuetify'
+import store from './store';
+import './registerServiceWorker.ts';
+import 'vuetify/dist/vuetify.min.css';
+
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount('#app');
