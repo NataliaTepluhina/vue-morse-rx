fc1c1a50111e6168514df8412cd42230a9ef5ece
NataliaTepluhina <tarya.se@gmail.com>
2018-03-12 09:14:57 +0200

    * added Rx to class-based Encode component

--- src/components/Encode.vue
+++ src/components/Encode.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
+    <div class="encode">
+        <v-btn raised dark class="deep-purple darken-4" v-stream:click="click$">Count</v-btn>
+        <v-btn raised dark class="deep-purple darken-4" @click="clearCounter()">Clear</v-btn>
+        <h2 v-if="counter">Clicks: {{counter}}</h2>
     </div>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator';
+import {Subject} from 'rxjs/Rx';
 
 @Component
 export default class Encode extends Vue {
-    @Prop() private msg: string;
+    public click$ = new Subject();
+    public counter: number = 0;
+
+    public mounted() {
+        this.$subscribeTo(
+            this.click$
+                .bufferWhen(() => this.click$.debounceTime(500)),
+            (clicksCount) => {
+                this.counter = clicksCount.length;
+            });
+    }
+    public clearCounter() {
+        this.counter = 0;
+    }
 }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import Vuetify from 'vuetify';
 import store from './store';
 import './registerServiceWorker.ts';
 import 'vuetify/dist/vuetify.min.css';
+import Rx from 'rxjs/Rx';
+import VueRx from 'vue-rx';
 
+Vue.use(VueRx, Rx);
 
 Vue.use(Vuetify);
 
